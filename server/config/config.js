f8446f9f445e14eef29fe1f83728e947e2931e33
Neil Martin <nnmartin94@gmail.com>
2019-10-20 12:48:14 -0700

    added config and env vars to sequelize connection

---
+++ server/config/config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  dbName: process.env.DB_NAME,
+  dbHost: process.env.DB_HOST,
+  dbUserName: process.env.DB_USERNAME,
+  dbPassword: process.env.DB_PASSWORD
+}
--- server/datamodels/index.js
+++ server/datamodels/index.js
@@ -1,2 +1,13 @@
-const sequelize = require('sequelize')
+const Sequelize = require('sequelize')
+const { dbName, dbUserName, dbPassword, dbHost } = require('../config/config')
 
+const sequelize = new Sequelize(dbName, dbUserName, dbPassword, {
+  host: dbHost,
+  dialect: 'postgres'
+})
+
+sequelize.authenticate()
+  .then(() => console.log('connection authenticated'))
+  .catch((e) => console.error(e))
+
+module.exports = { sequelize }
