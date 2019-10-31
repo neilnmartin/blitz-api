dec8ba1e79c7082fa7a68a33e74e083c335c2249
Neil Martin <nnmartin94@gmail.com>
2019-10-30 23:13:38 -0700

    added moment package to app

--- server/app.js
+++ server/app.js
@@ -1,17 +1,32 @@
 const express = require("express");
 const bodyParser = require('body-parser')
 const morgan = require("morgan");
-const router = require("./routes/");
+const config = require('./config/config')
 const { login, signup } = require("./routes/auth");
+const router = require("./routes/");
 const db = require('./database/');
 
 const app = express();
 
 app.use("/", morgan("dev"));
 app.use("/", bodyParser.json());
-app.use("/api", router);
 
 app.use("/login", login);
 app.use("/signup", signup);
+// verify token
+
+app.use("/api", async (req, res) => {
+  try {
+    var decoded = jwt.decode(req.header.token, config.auth.jwtSecret);
+    if (decoded.expiration < Date.now()) {
+      next()
+    } else {
+      res.status(401).send({ expiredToken: true })
+    }
+  } catch (e) {
+    console.error(e)
+  }
+}, router);
+
 
 module.exports = app;
