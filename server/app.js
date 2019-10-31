d9aa961fd2b262e6fa1b415a9b0f0f5d9f524bab
Neil Martin <nnmartin94@gmail.com>
2019-10-30 22:26:58 -0700

    added body parser

--- server/app.js
+++ server/app.js
@@ -1,11 +1,17 @@
 const express = require("express");
+const bodyParser = require('body-parser')
 const morgan = require("morgan");
-const router = require("./routes/index");
-const sequelize = require("./database/index");
+const router = require("./routes/");
+const { login, signup } = require("./routes/auth");
+const db = require('./database/');
 
 const app = express();
 
 app.use("/", morgan("dev"));
+app.use("/", bodyParser.json());
 app.use("/api", router);
+
+app.use("/login", login);
+app.use("/signup", signup);
 
 module.exports = app;
