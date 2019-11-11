4b8695849f32f113f1424f089224435fabfcd981
Neil Martin <nnmartin94@gmail.com>
2019-11-11 01:05:55 -0800

    added cors

--- server/app.js
+++ server/app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const bodyParser = require('body-parser');
 const morgan = require("morgan");
+const cors = require("cors");
 
 const verifyToken = require('./utils/verifyTokenMiddleware')
 
@@ -10,7 +11,9 @@ const router = require("./routes/");
 const app = express();
 
 app.use("/", morgan("dev"));
+app.use("/", cors());
 app.use("/", bodyParser.json());
+
 app.use("/auth", authRouter);
 app.use("/api", verifyToken, router);
 
