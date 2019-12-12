c8fea1f8d72e14d39381fb36bd791aa8c6482754
Neil Martin <nnmartin94@gmail.com>
2019-12-12 11:57:48 -0800

    added userControllers get req handler

--- server/controllers/userControllers.js
+++ server/controllers/userControllers.js
@@ -1,20 +1,31 @@
+const db = require("../database");
+
 module.exports = {
-  get: (req, res)  => {
-    console.log(req.params)
-    // db
-    res.send('hello from get')
+  get: async (req, res) => {
+    try {
+      console.log(req.params)
+      // db
+      const users = await db.query(`
+        SELECT id, email, username, password, first_name, last_name FROM users
+        WHERE email='${email}'`);
+      console.log("db users: ", users);
+      res.status(200).send(users)
+    } catch (e) {
+      console.error(e)
+      throw new Error(e)
+    }
   },
-  post: (req, res)  => {
+  post: (req, res) => {
     console.log(req.params)
     // db
     res.send('hello from post')
   },
-  put: (req, res)  => {
+  put: (req, res) => {
     console.log(req.params)
     // db
     res.send('hello from put')
   },
-  delete: (req, res)  => {
+  delete: (req, res) => {
     console.log(req.params)
     // db
     res.send('hello from delete')
